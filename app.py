52cb1c14c5cb4c6fbf9e82ad26714b2f5594b761
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-30 22:02:49 -0700

    users can create playlists

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -8,10 +8,7 @@ playlists = db.playlists
 app = Flask(__name__)
 
 # @app.route('/')
-# def index():
-#     """Return homepage."""
-#     return render_template('home.html', msg = 'Flask is cool!')
-
+# 
 # playlists = [
 #     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
 #     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
@@ -21,6 +18,27 @@ app = Flask(__name__)
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
+
+def index():
+    """Return homepage."""
+    return render_template('home.html', msg = 'Flask is cool!')
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description'),
+    'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
 
 
 if __name__ == '__main__':
